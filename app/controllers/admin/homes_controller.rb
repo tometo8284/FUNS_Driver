f6a0d2d9a448abd27e6e15e928fe67e6a9abd0b2
EC2 Default User <ec2-user@ip-172-31-14-67.ap-northeast-1.compute.internal>
2023-06-26 13:47:14 +0000

    投稿編集画面まで完成

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,6 @@ class Admin::HomesController < ApplicationController
   def top
     @q = Post.ransack(params[:q])
     @posts = @q.result(distinct: true).includes(:user).order("created_at desc")
+    @category_ranks = Category.find(Post.group(:category_id).order('count(category_id) desc').pluck(:category_id))
   end
 end
